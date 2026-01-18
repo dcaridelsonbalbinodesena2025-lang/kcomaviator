bd217bce33478935ea1e0573e791272d1ae87a7b
dcaridelsonbalbinodesena2025-lang <dc.aridelsonbalbinodesena2025@gmail.com>
2026-01-18 15:42:09 -0300

    main.py

--- main.py
+++ main.py
@@ -5,8 +5,8 @@ from flask_cors import CORS
 app = Flask(__name__)
 CORS(app)
 
-# Valor inicial
-ultima_vela = 0.0
+# Valor de teste para ver se aparece na sua calculadora
+ultima_vela = 2.50 
 
 @app.route('/')
 def home():
@@ -17,6 +17,6 @@ def get_ultimo():
     return jsonify({"valor": ultima_vela})
 
 if __name__ == "__main__":
-    # O Render exige que usemos a porta que ele fornece
+    # O Render exige que usemos a porta fornecida por eles
     port = int(os.environ.get("PORT", 10000))
     app.run(host='0.0.0.0', port=port)
