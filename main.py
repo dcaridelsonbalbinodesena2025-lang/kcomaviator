7b04e05eaba12ecc2c033cfd6f4cf6cfd2eb19c7
dcaridelsonbalbinodesena2025-lang <dc.aridelsonbalbinodesena2025@gmail.com>
2026-01-18 15:48:37 -0300

    main.py

--- main.py
+++ main.py
@@ -5,18 +5,18 @@ from flask_cors import CORS
 app = Flask(__name__)
 CORS(app)
 
-# Valor de teste para ver se aparece na sua calculadora
-ultima_vela = 2.50 
+# Valor de teste para confirmar que a calculadora recebe dados
+ultima_vela = 2.00 
 
 @app.route('/')
 def home():
-    return "Robo Online - Use /ultimo para dados"
+    return "Robo Online"
 
 @app.route('/ultimo')
 def get_ultimo():
     return jsonify({"valor": ultima_vela})
 
 if __name__ == "__main__":
-    # O Render exige que usemos a porta fornecida por eles
+    # Importante para o Render não dar erro de porta
     port = int(os.environ.get("PORT", 10000))
     app.run(host='0.0.0.0', port=port)
